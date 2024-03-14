460f036af4a64c9d96075005ce80c7e827a4f812
parthr01 <parth_rustagi@parths-mbp-2.wifi.local.cmu.edu>
2024-03-14 16:31:36 -0400

    NameAndLarge

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -6,6 +6,23 @@ export default function QueryProcessor(query: string): string {
       "writer in the English language and the world's pre-eminent dramatist."
     );
   }
+  if (query.toLowerCase().includes("name")) {
+    return (
+      "ParthR"
+    );
+  }
+
+  else if (query.toLowerCase().includes("largest")) {
+      const regex = /Which of the following numbers is the largest: (\d+), (\d+), (\d+)\?/;
+      const queryMatch = query.match(regex);
+      if (queryMatch) {
+        const num1 = parseInt(queryMatch[1]);
+        const num2 = parseInt(queryMatch[2]);
+        const num3 = parseInt(queryMatch[3]);
+        const result = Math.max(num1,num2,num3);
+        return result.toString();
+      }
+  }
 
   return "";
 }
